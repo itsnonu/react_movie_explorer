e51e53b6f1e9b237370af251c6323cc278300a73
Salvador <snuno236@gmail.com>
2026-03-27 01:52:27 -0700

    fixed $ issues before movie titles

--- app/components/movieCard.jsx
+++ app/components/movieCard.jsx
@@ -6,7 +6,7 @@ function MovieCard({movie}) {
     return (
         <div className = "movie_card">
             <img src = {`${IMG_BASE}${movie.poster_path}`} alt = {`${movie.title} poster`}/>
-            <h3>${movie.title}</h3>
+            <h3>{movie.title}</h3>
             <p>Release Date: {movie.release_date}</p>
             <p>Rating: {movie.vote_average}</p>
         </div>
